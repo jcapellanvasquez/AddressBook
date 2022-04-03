ebe012b946557542a591d4511a494d5748036b4c
jv.capellan06@gmail.com <jv.capellan06@gmail.com>
2022-04-03 18:12:43 -0400

    client list added
    * add http interceptor to set auth token
    
    * add clients list view

---
+++ app/src/app/core/models/Address.ts
@@ -0,0 +1,11 @@
+export interface Address {
+  id: number;
+  state: string;
+  city: string;
+  address: string;
+  address1: string;
+  zip: string;
+  clientId: number;
+  active: boolean;
+  userId: number;
+}
