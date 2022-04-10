f0fde355076f2bddb031e119f571dbf4525527fc
jv.capellan06@gmail.com <jv.capellan06@gmail.com>
2022-04-10 16:32:49 -0400

    update release config
    * add session guard
    
    * add API ip address

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SessionGuard} from "./core/guards/session.guard";
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./features/features.module').then( m => m.FeaturesModule)
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule),
   }
 ];
 
@@ -12,4 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
